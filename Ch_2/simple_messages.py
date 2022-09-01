56b289e1fdbe8e1b63334dde51d11dccef5ce023
Jim-Journey <jshookzlifting@gmail.com>
2022-08-31 20:08:06 -0500

    Commit

---
+++ Ch_2/simple_messages.py
@@ -0,0 +1,7 @@
+#Assign a message to a variable, print the message. Then change the value of the variable to a new message, and print the new message.
+
+first_message = "This is one of my first messages in a variable."
+print(first_message)
+
+first_message = "This is the changed value of my message variable."
+print(first_message)
